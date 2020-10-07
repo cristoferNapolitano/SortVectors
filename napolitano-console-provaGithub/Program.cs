872eefbbadadaaf97e29622ae20ae27e1446c69b
Cristofer Mattia Napolitano <CRISTOFER.NAPOLITANO@local.ittcesena.it>
2020-10-07 11:06:54 +0200

    first step

---
+++ napolitano-console-provaGithub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace napolitano_console_provaGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            array = new int[] { 3, 6, 26, 23, 34, 84, 27, 64, 39 ,83};
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Array.Sort(array);
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
